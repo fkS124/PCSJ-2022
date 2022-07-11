ce1ac230b48df2abe3bb65e122a14b485485db28
OZHER <ozherment@gmail.com>
2022-07-11 22:50:20 +0200

    clouds

--- src/background.py
+++ src/background.py
@@ -18,24 +18,27 @@ def load_a(path: str):
     return pg.image.load(path).convert_alpha()
 
 
+def darker(color: tuple[int, ...] | pg.Color, degree: int):
+    new_color = [color[0] + degree, color[1] + degree, color[2] + degree]
+    for idx, col in enumerate(new_color):
+        if col > 255:
+            new_color[idx] = 255
+        elif col < 0:
+            new_color[idx] = 0
+    return pg.Color(new_color)
+
+
 class CloudSprite(Object2d):
 
-    scaling = {
-        1: 0.2,
-        2: 0.3,
-        3: 0.4
-    }
-    scrolling = {
-        1: 2.5,
-        2: 5.5,
-        3: 7.5
-    }
-
-    def __init__(self, pos: tuple[int, int], tag: int):
+    def __init__(self, pos: tuple[int, int]):
         super(CloudSprite, self).__init__(pos, (100, 100))
-        self.tag = tag
-        self.surface = pg.Surface((random.randrange(2, 6)*100, 100 * self.scaling[tag]))
-        self.surface.fill((246, 246, 246))
+        self.vel = random.randrange(1, 3)
+        self.length = random.random()+2
+        self.scrolling = random.randint(2, 10)
+        self.surface = pg.Surface((random.randrange(200, 600), random.randrange(40, 80)))
+        color = 225
+        self.color = (color, color, color)
+        self.surface.fill(self.color)
         self.rect = self.surface.get_rect(center=pos)
         self.x = self.rect.x
         self.perspective_x = 0
@@ -43,7 +46,7 @@ class CloudSprite(Object2d):
         self.w, self.h = 1920, 1080
 
     def update(self, camera_dxy: vec = vec(0, 0)):
-        self.x += (1 + 1 / self.tag) - camera_dxy.x / self.scrolling[self.tag]
+        self.x += self.vel - camera_dxy.x / self.scrolling
         self.rect.x = round(self.x)
         if self.rect.x + self.perspective_x > self.w:
             self.x = -self.rect.w - abs(self.perspective_x)
@@ -60,13 +63,12 @@ class CloudSprite(Object2d):
 class Background:
 
     def __init__(self, n_layers: int = 4):
-        self.layers: list[list[Object2d]] = [[] for _ in range(n_layers)]
+        self.layers: list[Object2d] = []
 
     def draw(self, display: pg.Surface, camera_dxy: vec = vec(0, 0)):
-        for layer in self.layers:
-            for sprite in layer:
-                sprite.update(camera_dxy)
-                sprite.draw(display)
+        for sprite in self.layers:
+            sprite.update(camera_dxy)
+            sprite.draw(display)
 
 
 class NormalBackground(Background):
@@ -74,56 +76,52 @@ class NormalBackground(Background):
     def __init__(self):
         super(NormalBackground, self).__init__(3)
 
-        n_clouds = [3, 5, 6, 0]
-        for i in range(3):
-            for _ in n_clouds:
-                self.layers[i].append(
-                    CloudSprite((randint(0, 1400), randint(0, 700)), i+1)
+        for _ in range(7):
+            self.layers.append(
+                CloudSprite((randint(0, _*200), 100*_+80))
                 )
 
     def draw(self, display: pg.Surface, camera_dxy: vec = vec(0, 0)):
         vanishing_point = vec(display.get_size())/2
-        length3d = 5
-        for layer in self.layers:
-            for sprite in layer:
-                sprite.update(camera_dxy)
-                pos = vec(sprite.rect.topleft)
-                w, h = sprite.rect.w, sprite.rect.h
+        for sprite in self.layers:
+            sprite.update(camera_dxy)
+            pos = vec(sprite.rect.topleft)
+            w, h = sprite.rect.w, sprite.rect.h
 
-                vector = vanishing_point - pos
-                cond = vector[0] > w / 2, vector[1] > h / 2
-                way = ['bottom' if cond[1] else 'top', 'right' if cond[0] else 'left']
+            vector = vanishing_point - pos
+            cond = vector[0] > w / 2, vector[1] > h / 2
+            way = ['bottom' if cond[1] else 'top', 'right' if cond[0] else 'left']
 
-                vector -= vec(w * cond[0], h * cond[1])
-                pos += vec(w * cond[0], h * cond[1])
+            vector -= vec(w * cond[0], h * cond[1])
+            pos += vec(w * cond[0], h * cond[1])
 
-                vectors = {'left': vec(vector - vec(w, 0)) / length3d,
-                           'right': vec(vector + vec(w, 0)) / length3d,
-                           'top': vec(vector - vec(0, h)) / length3d,
-                           'bottom': vec(vector + vec(0, h)) / length3d}
+            vectors = {'left': vec(vector - vec(w, 0)) / sprite.length,
+                       'right': vec(vector + vec(w, 0)) / sprite.length,
+                       'top': vec(vector - vec(0, h)) / sprite.length,
+                       'bottom': vec(vector + vec(0, h)) / sprite.length}
 
-                vector /= length3d
-                sprite.perspective_x = vector.x
+            vector /= sprite.length
+            sprite.perspective_x = vector.x
 
-                point = {'left': vec(w, 0),
-                         'right': -vec(w, 0),
-                         'top': vec(0, h),
-                         'bottom': -vec(0, h)}
+            point = {'left': vec(w, 0),
+                     'right': -vec(w, 0),
+                     'top': vec(0, h),
+                     'bottom': -vec(0, h)}
 
-                colors = {
-                    'left': (200, 200, 200),
-                    'right': (200, 200, 200),
-                    'top': (200, 200, 200),
-                    'bottom': (200, 200, 200)}
+            colors = {
+                'left': darker(sprite.color, -20),
+                'right': darker(sprite.color, 20),
+                'top': darker(sprite.color, 40),
+                'bottom': darker(sprite.color, -40)}
 
-                conditions = {'left': vector[0] < 0,
-                              'right': vector[0] > 0,
-                              'top': vector[1] < 0,
-                              'bottom': vector[1] > 0}
-                for i in range(0, 2):
-                    if conditions[way[i]]:
-                        filled_polygon(display, (pos, pos + point[way[-i + 1]],
-                                                 pos + point[way[-i + 1]] + vectors[way[-i + 1]],
-                                                 pos + vector), colors[way[i]])
-                sprite.draw(display)
+            conditions = {'left': vector[0] < 0,
+                          'right': vector[0] > 0,
+                          'top': vector[1] < 0,
+                          'bottom': vector[1] > 0}
+            for i in range(0, 2):
+                if conditions[way[i]]:
+                    filled_polygon(display, (pos, pos + point[way[-i + 1]],
+                                             pos + point[way[-i + 1]] + vectors[way[-i + 1]],
+                                             pos + vector), colors[way[i]])
+            sprite.draw(display)
 
